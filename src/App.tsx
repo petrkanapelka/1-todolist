6b327f4fa48d22f78f609430cca6cfb053c61ccd
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-22 00:07:06 +0300

    feat: add task data to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
-import { ToDoList } from './components/toDoList/ToDoList';
+import { TaskType, ToDoList } from './components/toDoList/ToDoList';
 
 function App() {
+    const toDoListTitle: string = `What to learn?`;
+    const tasks: Array<TaskType> = [
+        {
+            id: 1,
+            title: 'HTML/CSS',
+            isDone: true
+        },
+        {
+            id: 2,
+            title: 'Javascript',
+            isDone: true
+        },
+        {
+            id: 3,
+            title: 'React',
+            isDone: false
+        },
+    ]
     return (
         <div className="App">
-            <ToDoList/>
+            <ToDoList title={toDoListTitle} tasks={tasks}/>
         </div>
     );
 }
