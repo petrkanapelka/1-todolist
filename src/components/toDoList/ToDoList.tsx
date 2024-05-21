3562a4e84c0e1720baff1b7403353ef78c84cb67
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-22 00:10:54 +0300

    feat: enhance ToDoList component with type safety and Button component

--- src/components/toDoList/ToDoList.tsx
+++ src/components/toDoList/ToDoList.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import { FC } from "react"
+import { Button } from "../button/Button"
 
-export const ToDoList = () => {
+type ToDoListPropsType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const ToDoList: FC<ToDoListPropsType> = ({ title, tasks }: ToDoListPropsType) => {
+
+    const taskElements: Array<JSX.Element> | JSX.Element = (tasks.length !== 0)
+        ? tasks.map((task, index) => {
+            return (
+                <li key='index'>
+                    <input type="checkbox" checked={task.isDone} />
+                    <span>{task.title}</span>
+                </li>
+            )
+        })
+        : <span>Your tasklist is empty</span>
+
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{title}</h3>
             <div>
                 <input />
-                <button>+</button>
+                <Button title="+" />
             </div>
             <ul>
-                <li><input type="checkbox" checked={true} /> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true} /> <span>JS</span></li>
-                <li><input type="checkbox" checked={false} /> <span>React</span></li>
+                {taskElements}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <Button title="All" />
+                <Button title="Active" />
+                <Button title="Completed" />
             </div>
         </div>
     );
