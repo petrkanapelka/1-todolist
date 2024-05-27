6b982b10e30a02cfd5c4c8bddbe3399233e2008f
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-27 22:50:41 +0300

    feat: add logic for remove and filter handlering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TaskType, ToDoList } from './components/toDoList/ToDoList';
 
+export type FilteredProps = 'all' | 'completed' | 'active'
+
 function App() {
     const toDoListTitle: string = `What to learn?`;
-    const tasks: Array<TaskType> = [
+    const tasks1: Array<TaskType> = [
         {
             id: 1,
             title: 'HTML/CSS',
@@ -20,10 +22,38 @@ function App() {
             title: 'React',
             isDone: false
         },
+        {
+            id: 4,
+            title: 'Typescript',
+            isDone: false
+        },
     ]
+
+    const [tasks, setTasks] = useState<Array<TaskType>>(tasks1);
+
+    const removeHandler = (id: number) => {
+        const newTasks = tasks.filter(el => el.id !== id)
+        setTasks(newTasks);
+    }
+
+    const [filter, setFilter] = useState('all')
+
+  let tasksForTodolist = tasks
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter(task => task.isDone === false)
+  }
+
+  if (filter === 'completed') {
+    tasksForTodolist = tasks.filter(task => task.isDone === true)
+  }
+
+  const changeFilter = (status: FilteredProps) => {
+    setFilter(status)
+  }
+
     return (
         <div className="App">
-            <ToDoList title={toDoListTitle} tasks={tasks}/>
+            <ToDoList title={toDoListTitle} tasks={tasksForTodolist} removeHandler={removeHandler} changeFilter={changeFilter} />
         </div>
     );
 }
