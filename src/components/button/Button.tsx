a0af5efb8398e760eb41748a48dd5885f79a0057
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-03 21:40:58 +0300

    feat: add disable property

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,10 +3,11 @@ import { FC } from "react";
 type ButtonPropsType = {
     title: string,
     onClick?: () => void
+    disable?: boolean
 }
 
-export const Button: FC<ButtonPropsType> = ({ title, onClick }: ButtonPropsType) => {
+export const Button: FC<ButtonPropsType> = ({ title, onClick, disable }: ButtonPropsType) => {
     return (
-        <button onClick={onClick}>{title}</button>
+        <button onClick={onClick} disabled={disable}>{title} </button>
     );
 };
