d4ab3c76fd946c81eee4bdce36aa230f2d6bface
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-04 23:16:05 +0300

    test: fix App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  const h3 = screen.getByText(/What to learn?/i);
+  expect(h3).toBeInTheDocument();
 });
