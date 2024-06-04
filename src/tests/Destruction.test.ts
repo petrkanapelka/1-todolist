9976ffab037bbda0eb34b01e7fa5f80b95a326ee
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-06-04 23:16:43 +0300

    test: creat Destruction test

---
+++ src/tests/Destruction.test.ts
@@ -0,0 +1,39 @@
+type UserType = {
+    name: string
+    age: number
+    adress: {
+        city: string
+        country: string
+    }
+}
+
+let user: UserType;
+
+beforeEach(()=>{
+    user = {
+        name: 'Ivan',
+        age: 13,
+        adress: {
+            city: "Minsk",
+            country: 'Belarus'
+        }
+    }
+})
+
+describe('Testing destructrion', ()=>{
+    test('should return Ivan',()=>{
+        const {name, ...rest} = user;
+        expect(name).toBe('Ivan')
+        expect(rest.age).toBe(13)
+    })
+
+    test('shoud return adress',()=>{
+        const {name, age, adress} = user;
+        expect(name).toBe('Ivan')
+        expect(age).toBe(13)
+        expect(adress).toStrictEqual({
+            city: "Minsk",
+            country: 'Belarus'
+        })
+    })
+})
