249cf0e71cc5cb8460f2ce0f8c6a623c875b3ef5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-04 20:20:51 +0300

    feat: update MenuButton

--- src/components/menuButton/MenuButton.tsx
+++ src/components/menuButton/MenuButton.tsx
@@ -7,7 +7,7 @@ type MenuButtonProps = {
 }
 
 export const MenuButton = styled(Button)<MenuButtonProps>(({ backgroundColor, theme }) => ({
-    backgroundColor: backgroundColor || theme.palette.primary.main,
+    //backgroundColor: backgroundColor || theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
     padding: '12px 24px',
     '&:hover': {
