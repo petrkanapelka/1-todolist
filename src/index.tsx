b0c57cb32864bc3ab5da260dd1125b9493f567d4
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-07-24 22:18:53 +0300

    feat: update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
+// import App from './App';
+// import AppWithReducer from './AppWithReducer';
+import AppWithRedux from './AppWithRedux';
+import { Provider } from 'react-redux';
+import { store } from './modules/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <AppWithReducer/> */}
+    <Provider store={store}>
+      <AppWithRedux />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
