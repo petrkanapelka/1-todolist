b4e1369437e5b9059648f77074b1f84804a6474d
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-03 00:30:41 +0300

    refactor: Task

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, memo, useCallback } from "react";
 import { EditableSpan } from "../editableSpan/EditableSpan";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Checkbox from '@mui/material/Checkbox';
 import ListItem from '@mui/material/ListItem';
-import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useDispatch } from "react-redux";
+import { changeTaskStatusAC, changeTaskTitleAC } from "../../modules/tasks-reducer";
 
 
 type TaskPropsType = {
@@ -12,28 +13,50 @@ type TaskPropsType = {
     taskID: string
     isDone: boolean
     tlID: string
-    changeTaskStatusHandler: (e: ChangeEvent<HTMLInputElement>) => void
-    updatedTasksHandler: (newTitle: string) => void
+    changeTaskStatusHandler: (e: ChangeEvent<HTMLInputElement>, taskId: string) => void
+    updatedTasksHandler: (newTitle: string, taskId: string) => void
     removeHandler: (taskID: string, tlID: string) => void
 
 };
-export const Task = (props: TaskPropsType) => {
-    const taskStyle = {
-        width: "100 %",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: '0'
-    }
 
-    const [liRef] = useAutoAnimate<HTMLLIElement>()
+const taskStyle = {
+    width: "100 %",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: '0'
+}
+
+export const Task = memo((props: TaskPropsType) => {
+
+    console.log('task called')
+
+
+    const dispatch = useDispatch();
+
+    // const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    //     props.changeTaskStatusHandler(e, props.taskID)
+    // }, [props])
+
+    // const updatedItemHandler = useCallback((newTitle: string) => {
+    //     props.updatedTasksHandler(newTitle, props.taskID)
+    // }, [props])
+
+    const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        dispatch(changeTaskStatusAC(props.taskID, e.currentTarget.checked, props.tlID))
+    }, [dispatch, props.taskID, props.tlID])
+
+    const updatedItemHandler = useCallback((newTitle: string) => {
+        dispatch(changeTaskTitleAC(props.taskID, newTitle, props.tlID))
+    }, [dispatch, props.taskID, props.tlID])
+
     return (
-        <ListItem key={props.taskID} className="task-item" style={taskStyle} ref={liRef}>
-            <Checkbox onChange={props.changeTaskStatusHandler} checked={props.isDone} />
-            <EditableSpan title={props.title} isDone={props.isDone} updatedItem={props.updatedTasksHandler} />
+        <ListItem key={props.taskID} className="task-item" style={taskStyle} >
+            <Checkbox onChange={onChangeHandler} checked={props.isDone} />
+            <EditableSpan title={props.title} isDone={props.isDone} updatedItem={updatedItemHandler} />
             <IconButton aria-label="delete" onClick={() => props.removeHandler(props.taskID, props.tlID)}>
                 <DeleteIcon />
             </IconButton>
         </ListItem>
     );
-};
+});
