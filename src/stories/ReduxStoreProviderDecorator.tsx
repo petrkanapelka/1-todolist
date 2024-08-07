3cdeee0b44897ea7e97eae57a1becc9e44911e56
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-08-08 00:55:03 +0300

    feat: create ReduxStoreProviderDecorator

---
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-useless-computed-key */
+import React from 'react'
+import { Provider } from "react-redux";
+import { combineReducers, legacy_createStore } from "redux";
+import { v1 } from "uuid";
+import { toDoListsReducer } from '../modules/todolists-reducer';
+import { tasksReducer } from '../modules/tasks-reducer';
+import { AppRootStateType } from '../modules/store';
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: toDoListsReducer
+})
+
+const initialGlobalState: any = {
+    tasks: {
+        ["todolistId1"]: [
+            { id: v1(), title: "HTML&CSS", isDone: true },
+            { id: v1(), title: "JS", isDone: false }
+        ],
+        ["todolistId2"]: [
+            { id: v1(), title: "Milk", isDone: false },
+            { id: v1(), title: "React Book", isDone: true }
+        ]
+    },
+    todolists: [
+        { id: "todolistId1", title: "What to learn", filter: "all" },
+        { id: "todolistId2", title: "What to buy", filter: "all" }
+    ],
+
+};
+
+export const storyBookStore = legacy_createStore(rootReducer, initialGlobalState);
+
+
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
+    return <Provider store={storyBookStore}>
+        {storyFn()}
+    </Provider>
+}
