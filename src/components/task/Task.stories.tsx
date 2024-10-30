366fba7f56cf5004581a19da4dd5479c4cdbb5a5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-10-30 21:01:28 +0300

    Removes Task stories
    Comments out all code related to Task stories.  This removes them from Storybook.

--- src/components/task/Task.stories.tsx
+++ src/components/task/Task.stories.tsx
@@ -1,63 +1,66 @@
-import { Task } from './Task';
-import { v1 } from 'uuid';
-import type { Meta, StoryObj } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { ReduxStoreProviderDecorator } from '../../stories/ReduxStoreProviderDecorator';
-import { useSelector } from 'react-redux';
-import { AppRootStateType } from '../../modules/store';
-import { TaskType } from '../toDoList/ToDoList';
-import { useState } from 'react';
+// import { Task } from './Task';
+// import { v1 } from 'uuid';
+// import type { Meta, StoryObj } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+// import { ReduxStoreProviderDecorator } from '../../stories/ReduxStoreProviderDecorator';
+// import { useSelector } from 'react-redux';
+// import { AppRootStateType } from '../../modules/store';
+// import { TaskType } from '../toDoList/ToDoList';
+// import { useState } from 'react';
 
-const tlID = v1();
-const taskID = v1();
+// const tlID = v1();
+// const taskID = v1();
 
-const meta: Meta<typeof Task> = {
-    title: 'TODOLISTS/Task',
-    component: Task,
-    decorators: [ReduxStoreProviderDecorator],
-    parameters: {
-        layout: 'centered',
-    },
-    tags: ['autodocs'],
-    args: {
-        changeTaskStatus: action('Task status changed'),
-        updateTaskTitle: action('Task title changed'),
-        removeHandler: action('Task deleted'),
-        taskID: taskID,
-        tlID: tlID,
-        title: 'Hello',
-        isDone: false
-    }
-};
+// const meta: Meta<typeof Task> = {
+//     title: 'TODOLISTS/Task',
+//     component: Task,
+//     decorators: [ReduxStoreProviderDecorator],
+//     parameters: {
+//         layout: 'centered',
+//     },
+//     tags: ['autodocs'],
+//     args: {
+//         changeTaskStatus: action('Task status changed'),
+//         updateTaskTitle: action('Task title changed'),
+//         removeHandler: action('Task deleted'),
+//         taskID: taskID,
+//         tlID: tlID,
+//         title: 'Hello',
+//         isDone: false
+//     }
+// };
 
-export default meta;
-type Story = StoryObj<typeof Task>;
+// export default meta;
+// type Story = StoryObj<typeof Task>;
 
 
 
-export const TaskIsNotDoneStory: Story = {
-};
+// export const TaskIsNotDoneStory: Story = {
+// };
 
-export const TaskIsDoneStory: Story = {
-    args: {
-        isDone: true
-    }
-};
-const TaskWithRedux = () => {
-    let task = useSelector<AppRootStateType, TaskType>(state => state.tasks['todolistId1'][0])
-    let [isDone, setIsDone] = useState(task.isDone)
-    return <Task
-        title={task.title}
-        isDone={isDone}
-        taskID={task.id}
-        tlID={'todolistId1'}
-        changeTaskStatus={() => setIsDone(!isDone)}
-        updateTaskTitle={action('Task title changed')}
-        removeHandler={action('Task deleted')} />
-}
+// export const TaskIsDoneStory: Story = {
+//     args: {
+//         isDone: true
+//     }
+// };
+// const TaskWithRedux = () => {
+//     let task = useSelector<AppRootStateType, TaskType>(state => state.tasks['todolistId1'][0])
+//     let [isDone, setIsDone] = useState(task.isDone)
+//     return <Task
+//         title={task.title}
+//         isDone={isDone}
+//         taskID={task.id}
+//         tlID={'todolistId1'}
+//         changeTaskStatus={() => setIsDone(!isDone)}
+//         updateTaskTitle={action('Task title changed')}
+//         removeHandler={action('Task deleted')} />
+// }
 
-export const TaskToggleStory = {
-    render: () => {
-        return <TaskWithRedux />
-    }
-}
+// export const TaskToggleStory = {
+//     render: () => {
+//         return <TaskWithRedux />
+//     }
+// }
+
+export default {}
+export const a = {}
