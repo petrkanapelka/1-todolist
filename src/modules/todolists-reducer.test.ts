aff8b20acafb657dceffc771691c97eccf7d21fd
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-10-31 23:37:18 +0300

    Removes unused test files
    Comments out the contents of several test files:
    
    - `tasks-reducer.test.ts`
    - `todolists-reducer.test.ts`
    - `todolists-tasks-reducer.test.ts`
    
    This prepares them for deletion in a subsequent commit.  A dummy export is added to each file to temporarily prevent TypeScript compiler errors due to empty files.

--- src/modules/todolists-reducer.test.ts
+++ src/modules/todolists-reducer.test.ts
@@ -1,58 +1,60 @@
-import { v1 } from 'uuid';
-import {
-    removeTodolistAC,
-    addTodolistAC,
-    changeTodolistTitleAC,
-    toDoListsReducer,
-    changeToDoListFilterAC,
-    ToDoListType,
-} from './todolists-reducer';
+// import { v1 } from 'uuid';
+// import {
+//     removeTodolistAC,
+//     addTodolistAC,
+//     changeTodolistTitleAC,
+//     toDoListsReducer,
+//     changeToDoListFilterAC,
+//     ToDoListType,
+// } from './todolists-reducer';
 
-let startState: ToDoListType[];
+// let startState: ToDoListType[];
 
-let toDoListId1: string;
-let todolistId2: string;
+// let toDoListId1: string;
+// let todolistId2: string;
 
-beforeEach(() => {
-    toDoListId1 = v1();
-    todolistId2 = v1();
+// beforeEach(() => {
+//     toDoListId1 = v1();
+//     todolistId2 = v1();
 
-    startState = [
-        { id: toDoListId1, title: 'What to learn', filter: 'all' },
-        { id: todolistId2, title: 'What to buy', filter: 'all' },
-    ];
-});
+//     startState = [
+//         { id: toDoListId1, title: 'What to learn', filter: 'all' },
+//         { id: todolistId2, title: 'What to buy', filter: 'all' },
+//     ];
+// });
 
-test('correct todolist should be removed', () => {
-    const action = removeTodolistAC(toDoListId1);
+// test('correct todolist should be removed', () => {
+//     const action = removeTodolistAC(toDoListId1);
 
-    const endState = toDoListsReducer(startState, action);
+//     const endState = toDoListsReducer(startState, action);
 
-    expect(endState.length).toBe(1);
-    expect(endState[0].id).toBe(todolistId2);
-});
+//     expect(endState.length).toBe(1);
+//     expect(endState[0].id).toBe(todolistId2);
+// });
 
-test('correct todolist should be added', () => {
-    const action = addTodolistAC('New Todolist');
+// test('correct todolist should be added', () => {
+//     const action = addTodolistAC('New Todolist');
 
-    const endState = toDoListsReducer(startState, action);
-    expect(endState.length).toBe(3);
-    expect(endState[0].title).toBe(action.payload.title);
-});
+//     const endState = toDoListsReducer(startState, action);
+//     expect(endState.length).toBe(3);
+//     expect(endState[0].title).toBe(action.payload.title);
+// });
 
-test('correct todolist should change its name', () => {
-    const action = changeTodolistTitleAC(todolistId2, 'New Todolist');
+// test('correct todolist should change its name', () => {
+//     const action = changeTodolistTitleAC(todolistId2, 'New Todolist');
 
-    const endState = toDoListsReducer(startState, action);
+//     const endState = toDoListsReducer(startState, action);
 
-    expect(endState[0].title).toBe('What to learn');
-    expect(endState[1].title).toBe('New Todolist');
-});
+//     expect(endState[0].title).toBe('What to learn');
+//     expect(endState[1].title).toBe('New Todolist');
+// });
 
-test('correct filter of todolist should be changed', () => {
-    const action = changeToDoListFilterAC(todolistId2, 'completed');
+// test('correct filter of todolist should be changed', () => {
+//     const action = changeToDoListFilterAC(todolistId2, 'completed');
 
-    const endState = toDoListsReducer(startState, action);
-    expect(endState[0].filter).toBe('all');
-    expect(endState[1].filter).toBe(action.payload.filter);
-});
+//     const endState = toDoListsReducer(startState, action);
+//     expect(endState[0].filter).toBe('all');
+//     expect(endState[1].filter).toBe(action.payload.filter);
+// });
+let a2 = 5;
+export { a2 };
