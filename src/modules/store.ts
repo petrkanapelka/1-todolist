f631c31214d5a81ed87f932261ecb5e24afd5f37
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-10-31 23:38:06 +0300

    Adds Redux Thunk middleware and typed hooks
    This commit integrates Redux Thunk middleware to enable asynchronous actions and introduces typed hooks for useDispatch and useSelector.  This improves the type safety and maintainability of asynchronous operations within the application.  It also simplifies the usage of dispatch and useSelector by providing type-safe access to the Redux store.

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,26 +1,33 @@
-import { tasksReducer } from './tasks-reducer';
-import { combineReducers, legacy_createStore } from 'redux';
-import { toDoListsReducer } from './todolists-reducer';
-import { loadState, saveState } from './localStorage';
-import { throttle } from 'lodash';
+import { tasksReducer } from "./tasks-reducer";
+import { applyMiddleware, combineReducers, legacy_createStore, UnknownAction } from "redux";
+import { toDoListsReducer } from "./todolists-reducer";
+import { loadState, saveState } from "./localStorage";
+import { throttle } from "lodash";
+import { thunk, ThunkDispatch } from "redux-thunk";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const persistedState = loadState();
 
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: toDoListsReducer,
+  tasks: tasksReducer,
+  todolists: toDoListsReducer,
 });
-export const store = legacy_createStore(rootReducer, persistedState);
+export const store = legacy_createStore(rootReducer, persistedState, applyMiddleware(thunk));
 export type AppRootStateType = ReturnType<typeof rootReducer>;
+export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, UnknownAction>;
+export const useAppDispatch = () => {
+  return useDispatch<AppDispatch>();
+};
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
 
 store.subscribe(() => {
-    saveState(store.getState());
+  saveState(store.getState());
 });
 
 store.subscribe(
-    throttle(() => {
-        saveState(store.getState());
-    }, 1000)
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000),
 );
 
 // @ts-ignore
