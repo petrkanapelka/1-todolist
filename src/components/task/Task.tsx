0a879ba320be04c8f120d43709a500f5fcfc32c4
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-01 00:27:46 +0300

    Refactors task removal logic
    Connects task removal to Redux toolkit and dispatches an async thunk.
    
    Simplifies the `Task` component by removing unnecessary props and using selectors. Improves performance by reducing rerenders and optimizing data flow.

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -5,6 +5,8 @@ import Checkbox from '@mui/material/Checkbox';
 import ListItem from '@mui/material/ListItem';
 import { DomainTask } from "./api/tasksApi.types";
 import { EditableSpan } from "components/editableSpan";
+import { useAppDispatch, useAppSelector } from "modules/store";
+import { removeTaskTC } from "modules/tasks-reducer";
 
 
 export type TaskPropsType = {
@@ -15,7 +17,7 @@ export type TaskPropsType = {
     tlID: string
     changeTaskStatus: (e: ChangeEvent<HTMLInputElement>, task: DomainTask) => void
     updateTaskTitle: (title: string, task: DomainTask) => void
-    removeHandler: (taskID: string, tlID: string) => void
+    // removeHandler: (taskID: string, tlID: string) => void
 
 };
 
@@ -28,26 +30,33 @@ const taskStyle = {
 }
 
 export const Task = memo((props: TaskPropsType) => {
+    const { taskID, tlID: todolistID } = props
+    const tasks = useAppSelector(state => state.tasks)
+    const dispatch = useAppDispatch()
+
+    const removeHandler = () => {
+        dispatch(removeTaskTC(taskID, todolistID))
+    }
 
     const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-        const task = props.tasks[props.tlID].find(task => task.id === props.taskID)
+        const task = tasks[todolistID].find(task => task.id === taskID)
         if (task) {
             props.changeTaskStatus(e, task)
         }
-    }, [props])
+    }, [props, taskID, tasks, todolistID])
 
     const updatedItemHandler = useCallback((newTitle: string) => {
-        const task = props.tasks[props.tlID].find(task => task.id === props.taskID)
+        const task = tasks[todolistID].find(task => task.id === taskID)
         if (task) {
             props.updateTaskTitle(newTitle, task)
         }
-    }, [props])
+    }, [props, taskID, tasks, todolistID])
 
     return (
         <ListItem key={props.taskID} className="task-item" style={taskStyle} >
             <Checkbox onChange={onChangeHandler} checked={props.isDone} />
             <EditableSpan title={props.title} isDone={props.isDone} updatedItem={updatedItemHandler} />
-            <IconButton aria-label="delete" onClick={() => props.removeHandler(props.taskID, props.tlID)}>
+            <IconButton aria-label="delete" onClick={removeHandler}>
                 <DeleteIcon />
             </IconButton>
         </ListItem>
