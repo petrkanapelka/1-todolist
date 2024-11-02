6a68c690ec47cc66b7bcc5630a44781f31773b4a
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-02 15:12:29 +0300

    Refactors tasks API functions
    Renames `todolistId` to `todoListId` for consistency.
    
    Updates `updateTask` to accept `UpdateTaskDomainModel` and return `ApiResponse<DomainTask>`.

--- src/components/task/api/tasksApi.ts
+++ src/components/task/api/tasksApi.ts
@@ -1,23 +1,23 @@
-import { DomainTask, GetTasksResponse, UpdateTaskModel } from './tasksApi.types';
-import { ApiResponse } from '../../../common/types/types';
-import { instance } from 'common/instance';
+import { DomainTask, GetTasksResponse, UpdateTaskDomainModel, UpdateTaskModel } from "./tasksApi.types";
+import { ApiResponse } from "../../../common/types/types";
+import { instance } from "common/instance";
 
 export const tasksApi = {
-    getTasks(todolistId: string) {
-        return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`);
-    },
-    createTask(payload: { title: string; todolistId: string }) {
-        const { title, todolistId } = payload;
-        return instance.post<ApiResponse<DomainTask>>(`todo-lists/${todolistId}/tasks`, { title });
-    },
+  getTasks(todoListId: string) {
+    return instance.get<GetTasksResponse>(`todo-lists/${todoListId}/tasks`);
+  },
+  createTask(payload: { title: string; todoListId: string }) {
+    const { title, todoListId } = payload;
+    return instance.post<ApiResponse<DomainTask>>(`todo-lists/${todoListId}/tasks`, { title });
+  },
 
-    deleteTask(payload: { taskId: string; todolistId: string }) {
-        const { taskId, todolistId } = payload;
-        return instance.delete<ApiResponse>(`todo-lists/${todolistId}/tasks/${taskId}`);
-    },
+  deleteTask(payload: { taskId: string; todoListId: string }) {
+    const { taskId, todoListId } = payload;
+    return instance.delete<ApiResponse>(`todo-lists/${todoListId}/tasks/${taskId}`);
+  },
 
-    updateTask(payload: { model: UpdateTaskModel; task: DomainTask }) {
-        const { model, task } = payload;
-        return instance.put<ApiResponse>(`todo-lists/${task.todoListId}/tasks/${task.id}`, model);
-    },
+  updateTask(payload: { model: UpdateTaskDomainModel; task: DomainTask }) {
+    const { model, task } = payload;
+    return instance.put<ApiResponse<DomainTask>>(`todo-lists/${task.todoListId}/tasks/${task.id}`, model);
+  },
 };
