f849ed50c679b5b2716bc06ad49ec4af1f92b022
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-03 14:18:49 +0300

    Adds global error snackbar
    Displays errors from anywhere in the application in a user-friendly snackbar.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './modules/store';
 import App from 'app/App';
+import { ErrorSnackbar } from 'components/ErrorSnackbar/ErrorSnackbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <App />
+    <ErrorSnackbar/>
   </Provider>
 );
 
