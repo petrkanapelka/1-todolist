d26012fd18dad83e8015c5c582f1bba914822696
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-03 23:47:28 +0300

    Adds initial routing configuration
    Sets up basic routing using `react-router-dom`.
    
    Includes routes for the main application layout (`App`) and two pages:
    - Main page (`/`)
    - Login page (`/login`)

---
+++ src/common/router/router.tsx
@@ -0,0 +1,25 @@
+import App from "app/App"
+import Main from "components/main/Main"
+import { Login } from "features/auth/Login"
+import { createBrowserRouter } from "react-router-dom"
+
+export const Path = {
+    Login: 'login',
+} as const
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Main />,
+            },
+            {
+                path: Path.Login,
+                element: <Login />,
+            },
+        ],
+    },
+])
