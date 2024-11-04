a6c6f412bee7db0b2d354893447130a0a033e0d6
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-04 21:34:24 +0300

    Implements login thunk
    Adds login thunk to handle user authentication.
    
    Dispatches actions to update app status and login state based on API response.
    Includes error handling for both server and network errors.

--- src/features/auth/model/auth-reducer.ts
+++ src/features/auth/model/auth-reducer.ts
@@ -1,6 +1,10 @@
 import { Dispatch } from "redux";
 import { LoginArgs } from "../api/authApi.types";
 import { setAppStatusAC } from "modules/app-reducer";
+import { authApi } from "../api/authApi";
+import { ResultCode } from "common/enums/enums";
+import { handleServerAppError } from "common/utils/handleServerAppError";
+import { handleServerNetworkError } from "common/utils/handleServerNetworkError";
 
 type InitialStateType = typeof initialState;
 
@@ -24,4 +28,18 @@ type ActionsType = ReturnType<typeof setIsLoggedInAC>;
 
 export const loginTC = (data: LoginArgs) => (dispatch: Dispatch) => {
   dispatch(setAppStatusAC("loading"));
+  debugger
+  authApi
+    .login(data)
+    .then((res) => {
+      if (res.data.resultCode === ResultCode.Success) {
+        dispatch(setIsLoggedInAC(true));
+        dispatch(setAppStatusAC("succeeded"));
+      } else {
+        handleServerAppError(res.data, dispatch);
+      }
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch);
+    });
 };
