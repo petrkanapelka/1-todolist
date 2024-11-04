004ddf175187414e711db0f362fda129e4f6ff8f
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-04 22:32:40 +0300

    Adds logout functionality
    Implements API endpoint for user logout.

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -6,4 +6,7 @@ export const authApi = {
   login(payload: LoginArgs) {
     return instance.post<ApiResponse<{ userId: number; token: string }>>(`auth/login`, payload);
   },
+  logout() {
+    return instance.delete<ApiResponse>('auth/login')
+  },
 };
