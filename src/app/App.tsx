090b024b682923e2f08dda4a23bdefc7bb1ed8c2
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-04 22:31:47 +0300

    Adds margin-top to App component
    Applies a 66px top margin to the App component's main div.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
     return (
         <>
-            <div className="App">
+            <div className="App" style={{marginTop:  '66px'}}>
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
                     <Header />
