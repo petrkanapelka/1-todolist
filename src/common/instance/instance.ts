3370112a44aa4e7b26923731bfb53d1aa5044840
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-04 22:07:34 +0300

    Uses localStorage token for API requests
    Intercepts API requests to replace the default bearer token with the token stored in localStorage.

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,9 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
-        'API-KEY': process.env.REACT_APP_API_KEY,
-    },
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
+    "API-KEY": process.env.REACT_APP_API_KEY,
+  },
+});
+
+instance.interceptors.request.use(function (config) {
+  config.headers["Authorization"] = `Bearer ${localStorage.getItem("sn-token")}`;
+
+  return config;
 });
