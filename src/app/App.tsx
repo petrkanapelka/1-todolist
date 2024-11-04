8b301633db4f93546b640091805c6a900a2586cc
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-04 22:42:39 +0300

    Initializes app state on mount
    Dispatches the `initializeAppTC` thunk action on component mount to initialize the application state.  This ensures necessary data is fetched and setup when the app loads.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,28 @@
+import './App.css';
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
-import { useAppSelector } from "modules/store";
-import './App.css';
+import { useAppDispatch, useAppSelector } from "modules/store";
 import { getTheme } from "common/theme/getTheme";
 import { ErrorSnackbar } from "components/ErrorSnackbar/ErrorSnackbar";
 import { Outlet } from "react-router-dom";
 import Header from "components/header/Header";
+import { initializeAppTC } from "features/auth/model/auth-reducer";
+import { useEffect } from "react";
 
 
 function App() {
     const themeMode = useAppSelector(state => state.app.themeMode)
     const theme = getTheme(themeMode)
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    }, [dispatch])
 
 
     return (
         <>
-            <div className="App" style={{marginTop:  '66px'}}>
+            <div className="App" style={{ marginTop: '66px' }}>
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
                     <Header />
