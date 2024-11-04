e68d6c6f7f83bf9126155bad0e8fa69b3c912558
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-04 21:34:08 +0300

    Refactors: Uses ApiResponse type in authApi
    Replaces the BaseResponse type with the more specific ApiResponse type in the login function of the authApi.

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,9 +1,9 @@
 import { instance } from "common/instance";
-import { BaseResponse } from "common/types/types";
+import { ApiResponse } from "common/types/types";
 import { LoginArgs } from "./authApi.types";
 
 export const authApi = {
   login(payload: LoginArgs) {
-    return instance.post<BaseResponse<{ userId: number; token: string }>>(`auth/login`, payload);
+    return instance.post<ApiResponse<{ userId: number; token: string }>>(`auth/login`, payload);
   },
 };
