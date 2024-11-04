5ec436a5eddcf921132bc997bd6ef85d6b2cd09f
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-04 21:19:13 +0300

    Implements login form functionality
    Adds form handling using `react-hook-form` library.  This includes:
    
    - Setting up form input fields for email, password, and remember me checkbox.
    - Handling form submission and logging the submitted data to the console.
    - Resetting the form after successful submission.
    
    This sets the foundation for user authentication.

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,13 +1,34 @@
 import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from "@mui/material"
 import { getTheme } from "common/theme/getTheme"
 import { useAppSelector } from "modules/store"
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import s from './Login.module.css'
+
+type Inputs = {
+    email: string
+    password: string
+    rememberMe: boolean
+}
 
 export const Login = () => {
     const themeMode = useAppSelector(state => state.app.themeMode)
     const theme = getTheme(themeMode)
 
+    const {
+        register,
+        handleSubmit,
+        reset,
+        control,
+        formState: { errors },
+    } = useForm<Inputs>({ defaultValues: { email: '', password: '', rememberMe: false } })
+
+    const onSubmit: SubmitHandler<Inputs> = data => {
+        console.log(data)
+        reset()
+    }
+
     return (
-        <Grid container justifyContent={'center'} sx={{marginTop: '15px'}}>
+        <Grid container justifyContent={'center'} sx={{ marginTop: '15px' }}>
             <Grid item justifyContent={'center'}>
                 <FormControl>
                     <FormLabel>
@@ -30,14 +51,26 @@ export const Login = () => {
                             <b>Password:</b> free
                         </p>
                     </FormLabel>
-                    <FormGroup>
-                        <TextField label="Email" margin="normal" />
-                        <TextField type="password" label="Password" margin="normal" />
-                        <FormControlLabel label={'Remember me'} control={<Checkbox />} />
-                        <Button type={'submit'} variant={'contained'} color={'primary'}>
-                            Login
-                        </Button>
-                    </FormGroup>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <FormGroup>
+                            <TextField label="Email" margin="normal" {...register('email')} />
+                            {errors.email && <span className={s.errorMessage}>{errors.email.message}</span>}
+                            <TextField type="password" label="Password" margin="normal" {...register('password')} />
+                            <FormControlLabel
+                                label={'Remember me'}
+                                control={
+                                    <Controller
+                                        name={'rememberMe'}
+                                        control={control}
+                                        render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
+                                    />
+                                }
+                            />
+                            <Button type={'submit'} variant={'contained'} color={'primary'} >
+                                Login
+                            </Button>
+                        </FormGroup>
+                    </form>
                 </FormControl>
             </Grid>
         </Grid>
