2211461c45d82de23bedcab48cd7d0e8635120d6
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-05 23:22:42 +0300

    Adds selectors for auth state
    Adds `selectIsLoggedIn` and `selectIsInitialized` selectors to the auth slice to provide convenient access to the authentication state.

--- src/features/auth/model/authSlice.ts
+++ src/features/auth/model/authSlice.ts
@@ -7,17 +7,20 @@ export const authSlice = createSlice({
     isInitialized: false,
   },
 
-  reducers: create => ({
+  reducers: (create) => ({
     setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
-      state.isLoggedIn = action.payload.isLoggedIn
+      state.isLoggedIn = action.payload.isLoggedIn;
     }),
     setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
-      state.isInitialized = action.payload.isInitialized
+      state.isInitialized = action.payload.isInitialized;
     }),
   }),
+  selectors: {
+    selectIsLoggedIn: (state) => state.isLoggedIn,
+    selectIsInitialized: (state) => state.isInitialized,
+  },
 });
-
-
 
 export const { setIsLoggedIn, setIsInitialized } = authSlice.actions;
 export const authReducer = authSlice.reducer;
+export const { selectIsInitialized, selectIsLoggedIn } = authSlice.selectors;
