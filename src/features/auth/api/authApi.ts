635d168e0c920975cf63af319c5a4c3b874a9d70
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-07 08:12:33 +0300

    Refactors auth API to use RTK Query
    Migrates auth API calls to use RTK Query for better caching, data management, and reduced boilerplate.
    
    Adds `useMeQuery`, `useLoginMutation`, and `useLogoutMutation` hooks for easy integration with components.

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,8 +1,9 @@
-import { instance } from "common/instance";
 import { ApiResponse } from "common/types/types";
 import { LoginArgs } from "./authApi.types";
+import { baseApi } from "app/baseApi";
+import { instance } from "common/instance";
 
-export const authApi = {
+export const _authApi = {
   login(payload: LoginArgs) {
     return instance.post<ApiResponse<{ userId: number; token: string }>>(`auth/login`, payload);
   },
@@ -13,3 +14,30 @@ export const authApi = {
     return instance.get<ApiResponse<{ id: number; email: string; login: string }>>('auth/me')
   },
 };
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    me: build.query<ApiResponse<{ id: number; email: string; login: string }>, void>({
+      query: () => "auth/me",
+    }),
+    login: build.mutation<ApiResponse<{ userId: number; token: string }>, LoginArgs>({
+      query: (payload) => {
+        return {
+          method: "POST",
+          url: "auth/login",
+          body: payload,
+        };
+      },
+    }),
+    logout: build.mutation<ApiResponse, void>({
+      query: () => {
+        return {
+          method: "DELETE",
+          url: "auth/login",
+        };
+      },
+    }),
+  }),
+});
+
+export const { useMeQuery, useLoginMutation, useLogoutMutation } = authApi;
