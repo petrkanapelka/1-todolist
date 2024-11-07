499f6951674ab0254a2b9d79a471a59acf91a59d
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-07 07:13:54 +0300

    Refactors todolists API to use RTK Query
    Migrates todolists API from custom implementation to RTK Query. This improves performance, code maintainability, and provides built-in caching and data fetching capabilities.
    
    Adds API key and token handling to the requests.  Transforms the response to include necessary properties for state management.

--- src/components/toDoList/api/todolistsApi.ts
+++ src/components/toDoList/api/todolistsApi.ts
@@ -1,19 +1,57 @@
 import { ApiResponse } from "common/types";
 import { DomainTodolist, Todolist } from "./todolistsApi.types";
-import { instance } from "common/instance";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const todolistsApi = {
-  getTodolists() {
-    return instance.get<DomainTodolist[]>("todo-lists");
-  },
-  updateTodolist(payload: { title: string; id: string }) {
-    const { title, id } = payload;
-    return instance.put<ApiResponse>(`todo-lists/${id}`, { title });
-  },
-  createTodolist(title: string) {
-    return instance.post<ApiResponse<{item:Todolist}>>("todo-lists", { title });
-  },
-  deleteTodolist(id: string) {
-    return instance.delete<ApiResponse>(`todo-lists/${id}`);
-  },
-};
+export const todolistsApi = createApi({
+  reducerPath: "todolistsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_BASE_URL,
+    prepareHeaders: (headers) => {
+      headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`);
+      headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`);
+    },
+  }),
+  endpoints: (build) => ({
+    getTodolists: build.query<any[], void>({
+      query: () => "todo-lists",
+      transformResponse(todolists: Todolist[]): DomainTodolist[] {
+        return todolists.map((tl) => ({ ...tl, filter: "all", entityStatus: "idle" }));
+      },
+    }),
+    addTodolist: build.mutation<ApiResponse<{ item: Todolist }>, string>({
+      query: (title) => {
+        return {
+          url: "todo-lists",
+          method: "POST",
+          body: { title },
+        };
+      },
+    }),
+    removeTodolist: build.mutation<ApiResponse, string>({
+      query: (id) => {
+        return {
+          method: "DELETE",
+          url: `todo-lists/${id}`,
+        };
+      },
+    }),
+    updateTodolistTitle: build.mutation<ApiResponse, { id: string; title: string }>({
+      query: ({ id, title }) => {
+        return {
+          method: "PUT",
+          url: `todo-lists/${id}`,
+          body: {
+            title,
+          },
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetTodolistsQuery,
+  useAddTodolistMutation,
+  useRemoveTodolistMutation,
+  useUpdateTodolistTitleMutation,
+} = todolistsApi;
