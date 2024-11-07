b4ef81299092210aa510ea95de337d3287c2260c
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-07 07:14:30 +0300

    Refactors state management with Redux Toolkit and RTK Query
    Migrates state management to use Redux Toolkit's `configureStore` and `createSlice`.  This improves type safety and reduces boilerplate.
    
    Integrates RTK Query for API interaction with todo lists, simplifying data fetching and caching.
    
    Removes reliance on `loadState` for persisted state as it's handled differently with RTK Query.

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,31 +1,38 @@
 import { combineReducers } from "redux";
-import { loadState, saveState } from "./localStorage";
+import { saveState } from "./localStorage";
 import { throttle } from "lodash";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer } from "features/auth/model/authSlice";
-import { appReducer } from "features/app/appSlice";
-import { toDoListsReducer } from "components/toDoList/model/todolistsSlice";
-import { tasksReducer } from "components/task/model/tasksSlice";
-
-const persistedState = loadState();
+import { authReducer, authSlice } from "features/auth/model/authSlice";
+import { appReducer, appSlice } from "features/app/appSlice";
+import { toDoListsReducer, todolistsSlice } from "components/toDoList/model/todolistsSlice";
+import { tasksReducer, tasksSlice } from "components/task/model/tasksSlice";
+import { todolistsApi } from "components/toDoList/api";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: toDoListsReducer,
-  app: appReducer,
-  auth: authReducer,
+  [tasksSlice.name]: tasksReducer,
+  [todolistsSlice.name]: toDoListsReducer,
+  [appSlice.name]: appReducer,
+  [authSlice.name]: authReducer,
+  [todolistsApi.reducerPath]: todolistsApi.reducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  preloadedState: persistedState,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
 });
+
+setupListeners(store.dispatch);
+
 export type AppRootStateType = ReturnType<typeof rootReducer>;
+
 export type AppDispatch = typeof store.dispatch;
+
 export const useAppDispatch = () => {
   return useDispatch<AppDispatch>();
 };
+
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
 
 store.subscribe(() => {
