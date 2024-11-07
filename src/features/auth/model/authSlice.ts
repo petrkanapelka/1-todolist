7940d639de07cc54836c4d3c26d8c6175239ac2e
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-07 08:12:49 +0300

    Removes auth slice and related logic
    Comments out the entire auth slice, including reducers, actions, and selectors.  This effectively removes the Redux-based authentication logic from the application.

--- src/features/auth/model/authSlice.ts
+++ src/features/auth/model/authSlice.ts
@@ -1,26 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-export const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    isLoggedIn: false,
-    isInitialized: false,
-  },
+// export const authSlice = createSlice({
+//   name: "auth",
+//   initialState: {
+//     isLoggedIn: false,
+//     isInitialized: false,
+//   },
 
-  reducers: (create) => ({
-    setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
-      state.isLoggedIn = action.payload.isLoggedIn;
-    }),
-    setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
-      state.isInitialized = action.payload.isInitialized;
-    }),
-  }),
-  selectors: {
-    selectIsLoggedIn: (state) => state.isLoggedIn,
-    selectIsInitialized: (state) => state.isInitialized,
-  },
-});
+//   reducers: (create) => ({
+//     setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
+//       state.isLoggedIn = action.payload.isLoggedIn;
+//     }),
+//     setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
+//       state.isInitialized = action.payload.isInitialized;
+//     }),
+//   }),
+//   selectors: {
+//     selectIsLoggedIn: (state) => state.isLoggedIn,
+//     selectIsInitialized: (state) => state.isInitialized,
+//   },
+// });
 
-export const { setIsLoggedIn, setIsInitialized } = authSlice.actions;
-export const authReducer = authSlice.reducer;
-export const { selectIsInitialized, selectIsLoggedIn } = authSlice.selectors;
+// export const { setIsLoggedIn, setIsInitialized } = authSlice.actions;
+// export const authReducer = authSlice.reducer;
+// export const { selectIsInitialized, selectIsLoggedIn } = authSlice.selectors;
+export {};
