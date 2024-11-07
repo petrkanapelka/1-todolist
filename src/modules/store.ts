e3372b6a976cc9605c40e2583250e63277485aa2
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-07 08:25:52 +0300

    Removes unused auth reducer import and code
    Removes the commented-out import and usage of the auth reducer and slice from the store configuration.  This cleans up the code and removes unnecessary clutter.

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -3,7 +3,6 @@ import { saveState } from "./localStorage";
 import { throttle } from "lodash";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-// import { authReducer, authSlice } from "features/auth/model/authSlice";
 import { appReducer, appSlice } from "features/app/appSlice";
 import { toDoListsReducer, todolistsSlice } from "components/toDoList/model/todolistsSlice";
 import { tasksReducer, tasksSlice } from "components/task/model/tasksSlice";
@@ -14,7 +13,6 @@ const rootReducer = combineReducers({
   [tasksSlice.name]: tasksReducer,
   [todolistsSlice.name]: toDoListsReducer,
   [appSlice.name]: appReducer,
-  // [authSlice.name]: authReducer,
   [baseApi.reducerPath]: baseApi.reducer,
 });
 
